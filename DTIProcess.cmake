1ddf2416255b52effc4763a449b5efb0cacc5ab9
Francois Budin <fbudin@unc.edu>
2013-01-17 21:17:13 +0000

    ENH: Modification of the configuration process. It now uses the structure of DTIPrep

---
+++ DTIProcess.cmake
@@ -0,0 +1,121 @@
+#-----------------------------------------------------------------------------
+set(MODULE_NAME ${EXTENSION_NAME}) # Do not use 'project()'
+set(MODULE_TITLE ${MODULE_NAME})
+
+string(TOUPPER ${MODULE_NAME} MODULE_NAME_UPPER)
+
+
+## A simple macro to set variables ONLY if it has not been set
+## This is needed when stand-alone packages are combined into
+## a larger package, and the desired behavior is that all the
+## binary results end up in the combined directory.
+if(NOT SETIFEMPTY)
+macro(SETIFEMPTY)
+  set(KEY ${ARGV0})
+  set(VALUE ${ARGV1})
+  if(NOT ${KEY})
+    set(${KEY} ${VALUE})
+  endif(NOT ${KEY})
+endmacro(SETIFEMPTY KEY VALUE)
+endif(NOT SETIFEMPTY)
+###
+SETIFEMPTY(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/lib)
+SETIFEMPTY(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/lib)
+SETIFEMPTY(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/bin)
+SETIFEMPTY(INSTALL_RUNTIME_DESTINATION bin)
+SETIFEMPTY(INSTALL_LIBRARY_DESTINATION lib)
+SETIFEMPTY(INSTALL_ARCHIVE_DESTINATION lib/static)
+SETIFEMPTY(CLI_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY} )
+SETIFEMPTY(CLI_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_LIBRARY_OUTPUT_DIRECTORY} )
+SETIFEMPTY(CLI_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_ARCHIVE_OUTPUT_DIRECTORY} )
+SETIFEMPTY(CLI_INSTALL_RUNTIME_DESTINATION ${INSTALL_RUNTIME_DESTINATION} )
+SETIFEMPTY(CLI_INSTALL_LIBRARY_DESTINATION ${INSTALL_LIBRARY_DESTINATION} )
+SETIFEMPTY(CLI_INSTALL_ARCHIVE_DESTINATION ${INSTALL_ARCHIVE_DESTINATION} )
+
+option(BUILD_dwiAtlas "Build dwiAtlas or not.  Requires boost." OFF)
+option(BUILD_TESTING "Build the testing tree" ON)
+
+##  In many cases sub-projects depending on SlicerExectuion Model
+##  that can be built stand alone are combined in larger packages.
+##  This logic will include SlicerExectionModel only if it
+##  has not already been included by a previous package.
+
+find_package(SlicerExecutionModel REQUIRED)
+include(${SlicerExecutionModel_USE_FILE})
+
+if(NOT ITK_FOUND)
+    find_package(ITK REQUIRED)
+    include(${ITK_USE_FILE})
+else()
+  if( NOT DEFINED ITKV3_COMPATIBILITY OR NOT ${ITKV3_COMPATIBILITY}  )
+    message( WARNING "Choose ITKv4 compiled with ITKV3_COMPATIBILITY set to ON (or GenerateCLP compiled against such an ITK version). If not, you may have compilation errors" )
+  endif()
+endif(NOT ITK_FOUND)
+
+
+if(NOT VTK_FOUND)
+    find_package(VTK REQUIRED)
+    include(${VTK_USE_FILE})
+endif(NOT VTK_FOUND)
+
+##  In many cases stand-alone sub-projects include private versions
+##  of DicomToNrrd
+##  that can be built stand alone are combined in larger packages.
+##  This logic will include DicomToNrrd only if it
+##  has not already been included by a previous package.
+if(NOT ADDFIRSTINSTANCE_DIRECTORY)
+macro(ADDFIRSTINSTANCE_DIRECTORY PROJECT_NAMESPACE STANDALONENAME)
+  if(BUILD${STANDALONENAME}FROM${PROJECT_NAMESPACE} OR NOT ${STANDALONENAME}_ALREADYINCLUDED)
+    set(BUILD${STANDALONENAME}FROM${PROJECT_NAMESPACE} ON CACHE BOOL "FLAG FOR ${STANDALONENAME} building to prevent recursion.")
+    set(${STANDALONENAME}_ALREADYINCLUDED ON CACHE BOOL "FLAG FOR ${STANDALONENAME} to indicate that it is already included.")
+    add_subdirectory(${STANDALONENAME})
+  endif( BUILD${STANDALONENAME}FROM${PROJECT_NAMESPACE} OR NOT ${STANDALONENAME}_ALREADYINCLUDED)
+endmacro(ADDFIRSTINSTANCE_DIRECTORY)
+endif(NOT ADDFIRSTINSTANCE_DIRECTORY)
+###
+
+INCLUDE_DIRECTORIES(
+${DTIProcess_SOURCE_DIR}/Library
+${DTIProcess_SOURCE_DIR}/PrivateLibrary
+${DTIProcess_SOURCE_DIR}
+)
+
+## Replace bessel(FORTRAN) with cephes(C)
+SET(BESSEL_LIB cephes)
+ADD_SUBDIRECTORY(cephes)
+
+ADD_SUBDIRECTORY(Library)
+ADD_SUBDIRECTORY(PrivateLibrary)
+ADD_SUBDIRECTORY(Applications)
+
+if( EXTENSION_SUPERBUILD_BINARY_DIR )
+  set( ITK_DIR_TMP ${ITK_DIR} )
+  unset( ITK_DIR CACHE )
+  unset( ITK_DIR )
+  set( SlicerExecutionModel_DIR_TMP ${SlicerExecutionModel_DIR} )
+  unset( SlicerExecutionModel_DIR )
+  unset( SlicerExecutionModel_DIR CACHE )
+  find_package(Slicer REQUIRED)
+  include(${Slicer_USE_FILE})
+  set( ITK_DIR ${ITK_DIR_TMP} CACHE PATH "ITK PATH" FORCE )
+  set( SlicerExecutionModel_DIR ${SlicerExecutionModel_DIR_TMP} CACHE PATH "SlicerExecutionModel PATH" FORCE )
+endif()
+
+IF(BUILD_TESTING)
+  include( CTest )
+  ADD_SUBDIRECTORY(Testing)
+ENDIF(BUILD_TESTING)
+
+
+
+set( Tools  dtiaverage dtiestim dtiprocess fiberprocess fiberstats fibertrack
+maxcurvature scalartransform )
+foreach( tool ${Tools} )
+  install(PROGRAMS ${CLI_RUNTIME_OUTPUT_DIRECTORY}/${tool}
+DESTINATION ${CLI_INSTALL_RUNTIME_DESTINATION})
+endforeach()
+
+if( EXTENSION_SUPERBUILD_BINARY_DIR )
+  set(CPACK_INSTALL_CMAKE_PROJECTS "${CPACK_INSTALL_CMAKE_PROJECTS};${CMAKE_BINARY_DIR};${EXTENSION_NAME};ALL;/")
+  include(${Slicer_EXTENSION_CPACK})
+endif()
--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -1,5 +1,14 @@
-cmake_minimum_required(VERSION 2.8.7)
+#-----------------------------------------------------------------------------
+set(LOCAL_PROJECT_NAME DTIProcess)
+#-----------------------------------------------------------------------------
+set(verbose FALSE)
+#-----------------------------------------------------------------------------
 
+
+
+unset( SlicerExecutionModel_DIR CACHE )
+unset( VTK_DIR CACHE )
+unset( ITK_DIR CACHE )
 
 option(USE_GIT_PROTOCOL "If behind a firewall turn this off to use http instead." ON)
 set(git_protocol "git")
@@ -9,6 +18,13 @@ else(NOT USE_GIT_PROTOCOL)
   set(git_protocol "git")
 endif()
 
+#-----------------------------------------------------------------------------
+# Enable and setup External project global properties
+#-----------------------------------------------------------------------------
+include(ExternalProject)
+include(SlicerMacroEmptyExternalProject)
+include(SlicerMacroCheckExternalProjectDependency)
+
 include(ExternalProject)
 if(CMAKE_EXTRA_GENERATOR)
   set(gen "${CMAKE_EXTRA_GENERATOR} - ${CMAKE_GENERATOR}")
@@ -16,86 +32,247 @@ else()
   set(gen "${CMAKE_GENERATOR}")
 endif()
 
-
 #-----------------------------------------------------------------------------
-unset( VTK_DIR CACHE )
-unset( VTK_DIR )
-unset( ITK_DIR CACHE )
-unset( ITK_DIR )
-unset( SlicerExecutionModel_DIR CACHE )
-unset( SlicerExecutionModel_DIR )
-find_package(Slicer REQUIRED)
-include(${Slicer_USE_FILE})
+# Platform check
+#-----------------------------------------------------------------------------
+
+set(PLATFORM_CHECK true)
+
+if(PLATFORM_CHECK)
+  # See CMake/Modules/Platform/Darwin.cmake)
+  #   6.x == Mac OSX 10.2 (Jaguar)
+  #   7.x == Mac OSX 10.3 (Panther)
+  #   8.x == Mac OSX 10.4 (Tiger)
+  #   9.x == Mac OSX 10.5 (Leopard)
+  #  10.x == Mac OSX 10.6 (Snow Leopard)
+  if (DARWIN_MAJOR_VERSION LESS "9")
+    message(FATAL_ERROR "Only Mac OSX >= 10.5 are supported !")
+  endif()
+endif()
+
+
+
+
+
 #-----------------------------------------------------------------------------
 # Project dependencies
 #-----------------------------------------------------------------------------
 
-set(proj Insight)
-set(${proj}_REPOSITORY ${git_protocol}://itk.org/ITK.git CACHE STRING "" FORCE)
-ExternalProject_Add(${proj}
-  GIT_TAG v4.3.0
-  GIT_REPOSITORY ${${proj}_REPOSITORY}
-  UPDATE_COMMAND ""
-  SOURCE_DIR ${proj}
-  BINARY_DIR ${proj}-build
-  CMAKE_GENERATOR ${gen}
-  CMAKE_ARGS
-    -DBUILD_SHARED_LIBS:BOOL=OFF
-    -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
-    -DITKV3_COMPATIBILITY:BOOL=ON
-    -DBUILD_TESTING:BOOL=OFF
-    -DBUILD_EXAMPLES:BOOL=OFF
-  INSTALL_COMMAND ""
+
+#-----------------------------------------------------------------------------
+# Superbuild option(s)
+#-----------------------------------------------------------------------------
+option(BUILD_STYLE_UTILS "Build uncrustify, cppcheck, & KWStyle" OFF)
+CMAKE_DEPENDENT_OPTION(
+  USE_SYSTEM_Uncrustify "Use system Uncrustify program" OFF
+  "BUILD_STYLE_UTILS" OFF
   )
-set(ITK4_DIR ${CMAKE_CURRENT_BINARY_DIR}/Insight-build)
-set(ITK4_DEPEND ${proj}) ## Set the internal dependancy for ITK
-
-
-set(proj SlicerExecutionModel)
-  ExternalProject_Add(${proj}
-  GIT_REPOSITORY ${git_protocol}://github.com/Slicer/SlicerExecutionModel.git
-  GIT_TAG "5ac91a89bba50db8b4f5e32cadbcb4baadb1ffc0"
-  SOURCE_DIR ${proj}
-  BINARY_DIR ${proj}-build
-  DEPENDS ${ITK4_DEPEND}
-  CMAKE_GENERATOR ${gen}
-  CMAKE_ARGS
-    -DBUILD_SHARED_LIBS:BOOL=OFF
-    -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
-    -DITK_DIR:PATH=${ITK4_DIR}
-    INSTALL_COMMAND ""
+CMAKE_DEPENDENT_OPTION(
+  USE_SYSTEM_KWStyle "Use system KWStyle program" OFF
+  "BUILD_STYLE_UTILS" OFF
+  )
+CMAKE_DEPENDENT_OPTION(
+  USE_SYSTEM_Cppcheck "Use system Cppcheck program" OFF
+  "BUILD_STYLE_UTILS" OFF
   )
 
-set(SlicerExecutionModel4_DIR ${CMAKE_CURRENT_BINARY_DIR}/SlicerExecutionModel-build)
-set(SlicerExecutionModel4_DEPEND ${proj}) ## Set the internal dependancy for ITK
 
-  set(proj inner)
+########Since is it an extension we do not leave the choice to the user of what is compiled
+#option(USE_SYSTEM_ITK "Build using an externally defined version of ITK" OFF)
+#option(USE_SYSTEM_SlicerExecutionModel "Build using an externally defined version of SlicerExecutionModel"  OFF)
+#option(USE_SYSTEM_VTK "Build using an externally defined version of VTK" ON)
+
+set(USE_SYSTEM_ITK OFF)
+set(USE_SYSTEM_SlicerExecutionModel OFF)
+
+#------------------------------------------------------------------------------
+# ${LOCAL_PROJECT_NAME} dependency list
+#------------------------------------------------------------------------------
+
+set(ITK_EXTERNAL_NAME ITKv4)
+
+set(${LOCAL_PROJECT_NAME}_DEPENDENCIES ${ITK_EXTERNAL_NAME} SlicerExecutionModel )
+
+if(BUILD_STYLE_UTILS)
+  list(APPEND ${LOCAL_PROJECT_NAME}_DEPENDENCIES Cppcheck KWStyle Uncrustify)
+endif()
+
+
+#-----------------------------------------------------------------------------
+# Define Superbuild global variables
+#-----------------------------------------------------------------------------
+
+# This variable will contain the list of CMake variable specific to each external project
+# that should passed to ${CMAKE_PROJECT_NAME}.
+# The item of this list should have the following form: <EP_VAR>:<TYPE>
+# where '<EP_VAR>' is an external project variable and TYPE is either BOOL, STRING, PATH or FILEPATH.
+# TODO Variable appended to this list will be automatically exported in ${LOCAL_PROJECT_NAME}Config.cmake,
+# prefix '${LOCAL_PROJECT_NAME}_' will be prepended if it applies.
+set(${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARS)
+
+# The macro '_expand_external_project_vars' can be used to expand the list of <EP_VAR>.
+set(${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_ARGS) # List of CMake args to configure BRAINS
+set(${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARNAMES) # List of CMake variable names
+
+# Convenient macro allowing to expand the list of EP_VAR listed in ${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARS
+# The expanded arguments will be appended to the list ${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_ARGS
+# Similarly the name of the EP_VARs will be appended to the list ${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARNAMES.
+macro(_expand_external_project_vars)
+  set(${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_ARGS "")
+  set(${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARNAMES "")
+  foreach(arg ${${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARS})
+    string(REPLACE ":" ";" varname_and_vartype ${arg})
+    set(target_info_list ${target_info_list})
+    list(GET varname_and_vartype 0 _varname)
+    list(GET varname_and_vartype 1 _vartype)
+    list(APPEND ${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_ARGS -D${_varname}:${_vartype}=${${_varname}})
+    list(APPEND ${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARNAMES ${_varname})
+  endforeach()
+endmacro()
+
+
+
+#-----------------------------------------------------------------------------
+# Common external projects CMake variables
+#-----------------------------------------------------------------------------
+list(APPEND ${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARS
+  MAKECOMMAND:STRING
+  CMAKE_SKIP_RPATH:BOOL
+  CMAKE_BUILD_TYPE:STRING
+  BUILD_SHARED_LIBS:BOOL
+  CMAKE_CXX_COMPILER:PATH
+  CMAKE_CXX_FLAGS_RELEASE:STRING
+  CMAKE_CXX_FLAGS_DEBUG:STRING
+  CMAKE_CXX_FLAGS:STRING
+  CMAKE_C_COMPILER:PATH
+  CMAKE_C_FLAGS_RELEASE:STRING
+  CMAKE_C_FLAGS_DEBUG:STRING
+  CMAKE_C_FLAGS:STRING
+  CMAKE_SHARED_LINKER_FLAGS:STRING
+  CMAKE_EXE_LINKER_FLAGS:STRING
+  CMAKE_MODULE_LINKER_FLAGS:STRING
+  CMAKE_GENERATOR:STRING
+  CMAKE_EXTRA_GENERATOR:STRING
+  CMAKE_INSTALL_PREFIX:PATH
+  CMAKE_LIBRARY_OUTPUT_DIRECTORY:PATH
+  CMAKE_ARCHIVE_OUTPUT_DIRECTORY:PATH
+  CMAKE_RUNTIME_OUTPUT_DIRECTORY:PATH
+  CMAKE_BUNDLE_OUTPUT_DIRECTORY:PATH
+  CTEST_NEW_FORMAT:BOOL
+  MEMORYCHECK_COMMAND_OPTIONS:STRING
+  MEMORYCHECK_COMMAND:PATH
+  CMAKE_SHARED_LINKER_FLAGS:STRING
+  CMAKE_EXE_LINKER_FLAGS:STRING
+  CMAKE_MODULE_LINKER_FLAGS:STRING
+  SITE:STRING
+  BUILDNAME:STRING
+  )
+
+if(${LOCAL_PROJECT_NAME}_USE_QT)
+  list(APPEND ${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARS
+    ${LOCAL_PROJECT_NAME}_USE_QT:BOOL
+    QT_QMAKE_EXECUTABLE:PATH
+    QT_MOC_EXECUTABLE:PATH
+    QT_UIC_EXECUTABLE:PATH
+    )
+endif()
+
+_expand_external_project_vars()
+set(COMMON_EXTERNAL_PROJECT_ARGS ${${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_ARGS})
+SlicerMacroCheckExternalProjectDependency(${LOCAL_PROJECT_NAME})
+
+#-----------------------------------------------------------------------------
+# Set CMake OSX variable to pass down the external project
+#-----------------------------------------------------------------------------
+set(CMAKE_OSX_EXTERNAL_PROJECT_ARGS)
+if(APPLE)
+  list(APPEND CMAKE_OSX_EXTERNAL_PROJECT_ARGS
+    -DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES}
+    -DCMAKE_OSX_SYSROOT=${CMAKE_OSX_SYSROOT}
+    -DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET})
+endif()
+
+
+
+#-----------------------------------------------------------------------------
+unset( VTK_DIR CACHE )
+unset( VTK_DIR )
+set( ITK_DIR_TMP ${ITK_DIR} )
+unset( ITK_DIR CACHE )
+unset( ITK_DIR )
+set( SlicerExecutionModel_DIR_TMP ${SlicerExecutionModel_DIR} )
+unset( SlicerExecutionModel_DIR CACHE )
+unset( SlicerExecutionModel_DIR )
+find_package(Slicer REQUIRED)
+include(${Slicer_USE_FILE})
+set( ITK_DIR ${ITK_DIR_TMP} CACHE PATH "ITK PATH" FORCE )
+set( SlicerExecutionModel_DIR ${SlicerExecutionModel_DIR_TMP} CACHE PATH "SlicerExecutionModel PATH" FORCE )
+set( BUILD_SHARED_LIBS OFF)
+#-----------------------------------------------------------------------------
+# Add external project CMake args
+#-----------------------------------------------------------------------------
+list(APPEND ${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARS
+  BUILD_TESTING:BOOL
+  ITK_VERSION_MAJOR:STRING
+  ITK_DIR:PATH
+  VTK_DIR:PATH
+  GenerateCLP_DIR:PATH
+  SlicerExecutionModel_DIR:PATH
+  )
+
+_expand_external_project_vars()
+set(COMMON_EXTERNAL_PROJECT_ARGS ${${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_ARGS})
+
+
+if(verbose)
+  message("Inner external project args:")
+  foreach(arg ${${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_ARGS})
+    message("  ${arg}")
+  endforeach()
+endif()
+
+string(REPLACE ";" "^" ${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARNAMES "${${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARNAMES}")
+
+if(verbose)
+  message("Inner external project argnames:")
+  foreach(argname ${${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARNAMES})
+    message("  ${argname}")
+  endforeach()
+endif()
+
+#------------------------------------------------------------------------------
+# Configure and build
+#------------------------------------------------------------------------------
+
+  set(proj ${LOCAL_PROJECT_NAME})
   ExternalProject_Add(${proj}
     DOWNLOAD_COMMAND ""
     SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}
     BINARY_DIR ${proj}-build
-    DEPENDS  ${ITK4_DEPEND} ${SlicerExecutionModel4_DEPEND} ${VTK_DEPEND}
+    DEPENDS ${${LOCAL_PROJECT_NAME}_DEPENDENCIES}
     CMAKE_GENERATOR ${gen}
     CMAKE_ARGS
-      -DITK_DIR:PATH=${ITK4_DIR}
-      -DVTK_DIR:PATH=${VTK_DIR}
-      -DSlicerExecutionModel_DIR:PATH=${SlicerExecutionModel4_DIR}
-      -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
-      -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
       -DCLI_RUNTIME_OUTPUT_DIRECTORY:PATH=${SlicerExecutionModel_DEFAULT_CLI_RUNTIME_OUTPUT_DIRECTORY}
       -DCLI_INSTALL_RUNTIME_DESTINATION:PATH=${SlicerExecutionModel_DEFAULT_CLI_INSTALL_RUNTIME_DESTINATION}
       -DMIDAS_PACKAGE_EMAIL:STRING=${MIDAS_PACKAGE_EMAIL}
       -DMIDAS_PACKAGE_API_KEY:STRING=${MIDAS_PACKAGE_API_KEY}
-      -DADDITIONAL_C_FLAGS:STRING=${ADDITIONAL_C_FLAGS}
-      -DADDITIONAL_CXX_FLAGS:STRING=${ADDITIONAL_CXX_FLAGS}
       -DDTIProcess_BUILD_SLICER_EXTENSION:BOOL=OFF
       -DEXTENSION_NAME:STRING=${EXTENSION_NAME}
       -DEXTENSION_SUPERBUILD_BINARY_DIR:PATH=${${EXTENSION_NAME}_BINARY_DIR}
+      ${CMAKE_OSX_EXTERNAL_PROJECT_ARGS}
+      ${COMMON_EXTERNAL_PROJECT_ARGS}
       # Slicer
       -DSlicer_DIR:PATH=${Slicer_DIR}
     INSTALL_COMMAND ""
   )
 
-
+## Force rebuilding of the main subproject every time building from super structure
+ExternalProject_Add_Step(${proj} forcebuild
+    COMMAND ${CMAKE_COMMAND} -E remove
+    ${CMAKE_CURRENT_BUILD_DIR}/${proj}-prefix/src/${proj}-stamp/${proj}-build
+    DEPENDEES configure
+    DEPENDERS build
+    ALWAYS 1
+  )
 
 #-----------------------------------------------------------------------------
